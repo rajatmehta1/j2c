394671dc8e62c023038a7c52c383abb5d7debb22
rmehta81 <rmehta81@gatech.edu>
2018-12-17 21:10:11 -0500

    changed files

--- src/main/java/j2c/daos/QuestionListDao.java
+++ src/main/java/j2c/daos/QuestionListDao.java
@@ -4,6 +4,7 @@ import j2c.pojos.Answer;
 import j2c.pojos.Question;
 import j2c.pojos.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import java.sql.Connection;
@@ -11,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Component
@@ -217,5 +219,111 @@ public class QuestionListDao {
         }
     }
 
+
+
+    public List<Question> findQuestionsByRange(int qidStart, boolean pullAnswers){
+        String sql = "SELECT * FROM q_all WHERE qid > ? and is_active = 'Y'";
+        Connection conn = null;
+        List<Question> questions = new ArrayList<>();
+        try {
+            conn = this.jdbcTemplate.getDataSource().getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1,qidStart);
+            Question q = null;
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+
+                q = new Question(
+                        rs.getInt("qid"),
+                        rs.getString("q_text"),
+                        rs.getInt("topic_id"),
+                        rs.getInt("created_by")
+                );
+                if(pullAnswers)
+                    q.setFirstAnswer(
+                            (this.findAnswers(q.getId()) != null && this.findAnswers(q.getId()).size() > 0)
+                                    ? this.findAnswers(q.getId()).get(0): null );
+                questions.add(q);
+
+            }
+
+            rs.close();
+            ps.close();
+            return questions;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {}
+            }
+        }
+    }
+
+
+
+    @Cacheable("questions")
+    public HashMap<String, String> getAllQuestions() {
+        String sql = "SELECT * FROM q_all WHERE is_active='Y' order by update_time";
+        Connection conn = null;
+        HashMap<String, String> hmap = new HashMap<>();
+        try {
+            conn = this.jdbcTemplate.getDataSource().getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                hmap.put(rs.getString("q_text"), "PRESENT");
+            }
+
+            rs.close();
+            ps.close();
+            return hmap;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {}
+            }
+        }
+    }
+
+
+    public List<Topic> findTopics(String lang){
+        String sql = "SELECT * FROM topics where topic_lang = '" + lang + "'";
+        Connection conn = null;
+        List<Topic> tp = new ArrayList<>();
+        try {
+            conn = this.jdbcTemplate.getDataSource().getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            Topic q = null;
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+
+                q = new Topic(
+                        rs.getInt("topic_id"),
+                        rs.getString("topic_name"),
+                        rs.getString("topic_lang")
+                );
+
+                tp.add(q);
+
+            }
+
+            rs.close();
+            ps.close();
+            return tp;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {}
+            }
+        }
+    }
 
 }
